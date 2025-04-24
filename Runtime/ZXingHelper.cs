29a0a9a252ecf6e5f92270be91e4cd126c3319eb
Kzzzzzzz <450228418@qq.com>
2025-04-24 17:50:48 +0800

    添加zxing.net.0.16.10

---
+++ Runtime/ZXingHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+namespace KC
+{
+    public static partial class ZXingHelper
+    {
+        public static Texture2D Generate(string url,int size = 256)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions()
+                {
+                    CharacterSet = "UTF-8",
+                    Width = size,
+                    Height = size
+                }
+            };
+            var pixels = writer.Write(url);
+            var texture = new Texture2D(size, size);
+            texture.SetPixels32(pixels);
+            texture.Apply();
+            return texture;
+        }
+    }
+}
